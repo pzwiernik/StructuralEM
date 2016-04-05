1e97e0fcb98ab57bc8a3a7bbc845ed221e35068a
pzwiernik <piotr.zwiernik@gmail.com>
2016-04-05 17:38:02 +0200

    A new function quick.start to produce a quick starting point for the EM algorithm. Works well for big trees.

---
+++ R/quick.start.R
@@ -0,0 +1,11 @@
+# quickly compute the starting point
+# in the current version we use fastme.bal()
+# from teh ape package
+quick.start <- function(dat){
+  D <- ape::get.dist(cor(dat))
+  T0 <- ape::fastme.bal(D)
+  #since the algorithm may produce negative lengths
+  # we replace all those instances by zeros
+  T0$edge.length <- (T0$edge.length>0)*T0$edge.length
+  return(T0)
+}
